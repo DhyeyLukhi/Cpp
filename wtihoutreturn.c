f83e4e40e497eae55445989f6dd181c3e188dcd1
DhyeyLukhi <dhyeylukhi95@gmail.com>
2026-01-04 09:47:47 +0530

    A small testing done, with shocking results

---
+++ wtihoutreturn.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fun(void);
+
+int main(){
+    int a=10, b=20;
+    printf("%d", fun());
+
+    return 0;
+}
+int fun(){
+    int z;
+    // z = x++ * ++y;
+    // return z;
+}
